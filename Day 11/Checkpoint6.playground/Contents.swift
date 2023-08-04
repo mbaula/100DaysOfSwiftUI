64d375b753e76be6b1f973b3f8acb2af7499b44b
mbaula <markbaula.1@gmail.com>
2023-08-04 16:14:18 -0400

    checkpoint 6 completed

--- Day 11/Checkpoint6.playground/Contents.swift
+++ Day 11/Checkpoint6.playground/Contents.swift
@@ -1,3 +1,42 @@
 import UIKit
 
-var greeting = "Hello, playground"
+struct Car {
+    let model: String
+    let numOfSeats: Int
+    private(set) var currentGear = 1
+    
+    mutating func gearUp() {
+        if(currentGear < 11) {
+            currentGear += 1
+            print("Going up a gear! Current gear is: \(currentGear)")
+        }
+        else {
+            print("Cannot go up a gear! Current gear is: \(currentGear)")
+        }
+    }
+    
+    mutating func gearDown() {
+        if(currentGear > 1) {
+            currentGear -= 1
+            print("Going down a gear! Current gear is: \(currentGear)")
+        }
+        else {
+            print("Cannot go down a gear! Current gear is: \(currentGear)")
+        }
+    }
+    
+    init(model: String, numOfSeats: Int) {
+        self.model = model
+        self.numOfSeats = numOfSeats
+        self.currentGear = 1
+    }
+}
+
+var car = Car.init(model: "Ford Shelby GT350", numOfSeats: 4)
+car.gearUp()
+car.gearUp()
+car.gearUp()
+car.gearDown()
+car.gearDown()
+car.gearDown()
+print(car)
